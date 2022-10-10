8af948adc776e1ca15c4210aaed2d2bd925ca120
kerli <keli.pajusaar@voco.ee>
2022-10-10 17:40:12 +0300

    lesson 7 ostunupp

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const app = Vue.createApp({
     data() {
         return {
+            activeClass: true,
             cart:0,
             product: 'Socks',
+            brand: 'Vue Mastery',
             image: './assets/images/socks_blue.jpg',
-            inStock: true,
+            inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
@@ -14,7 +16,7 @@ const app = Vue.createApp({
     },
     methods: {
         addToCart() {
-          this.cart += 1
+            this.cart += 1
         },
         updateImage(variantImage) {
             this.image = variantImage
